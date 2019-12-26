b3c544993b19a734284e1ddb7d55346f9f962291
za0ne101 <za0ne78@hotmail.com>
2019-12-26 16:38:09 +0200

    Exercises from chapter 3

---
+++ src/com/company/chapter3/ComputerAndIntrerpretBMI.java
@@ -0,0 +1,30 @@
+package com.company.chapter3;
+import java.util.Scanner;
+
+public class ComputerAndIntrerpretBMI {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter weight in pounds: ");
+        double weight = input.nextDouble();
+
+        System.out.print("Enter height in inches: ");
+        double height = input.nextDouble();
+
+        final double KILOGRAMS_PER_POUND = 0.45359237;
+        final double METERS_PER_INCH = 0.0254;
+
+        //Compute BMI
+        double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+        double heightInMeters = height * METERS_PER_INCH;
+        double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+
+        //Display result
+        System.out.println("BMI is " + bmi );
+        if(bmi < 18.5 ){
+
+        }
+    }
+}
---
+++ src/com/company/chapter3/SimpleIfDemo.java
@@ -0,0 +1,19 @@
+package com.company.chapter3;
+
+import java.util.Scanner;
+
+public class SimpleIfDemo {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int number = input.nextInt();
+
+        if(number % 5 == 0) {
+            System.out.println("Hi Five");
+        }
+        if(number % 2 == 0) {
+            System.out.println("Hi Even");
+        }
+    }
+}
